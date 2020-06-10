f3d8813eab397979a5fcafdce378e38b7a354743
admin <d.art.shishkin@gmail.com>
2020-06-10 20:03:15 +0300

    Skipping JUnit tests (@Disabled) and older tests are fixed

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -13,11 +13,11 @@ class OwnerTest {
         owner.setTelephone("1111");
         assertAll("Properties Test",
                 () -> assertAll("Person Properties",
-                        () -> assertEquals("Kat", owner.getFirstName(), () -> "First name does not match"),
-                        () -> assertEquals("Shysh", owner.getLastName(), () -> "Last name does not match")),
+                        () -> assertEquals("Kate", owner.getFirstName(), () -> "First name does not match"),
+                        () -> assertEquals("Shyshkina", owner.getLastName(), () -> "Last name does not match")),
                 () -> assertAll("Owner Properties",
-                        () -> assertEquals("Kram", owner.getCity(), "city does not match"),
-                        () -> assertEquals("1234", owner.getTelephone(), "telephone does not match")
+                        () -> assertEquals("Kramatorsk", owner.getCity(), "city does not match"),
+                        () -> assertEquals("1111", owner.getTelephone(), "telephone does not match")
                 ));
     }
 }
--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -10,16 +10,16 @@ class PersonTest {
     void groupedAssertions() {
         Person person = new Person(1L, "Art", "Shyshkin");
         assertAll("Test props set",
-                () -> assertEquals(Long.valueOf(2L), person.getId(), "id not match"),
+                () -> assertEquals(Long.valueOf(1L), person.getId(), "id not match"),
                 () -> assertEquals("Art", person.getFirstName()),
-                () -> assertEquals("Shyshkin1", person.getLastName(), "last name does not match"));
+                () -> assertEquals("Shyshkin", person.getLastName(), "last name does not match"));
     }
 
     @Test
     void notGroupedAssertions() {
         Person person = new Person(1L, "Art", "Shyshkin");
-        assertEquals(Long.valueOf(2L), person.getId(), "id not match");
+        assertEquals(Long.valueOf(1L), person.getId(), "id not match");
         assertEquals("Art", person.getFirstName());
-        assertEquals("Shyshkin1", person.getLastName(), "last name does not match");
+        assertEquals("Shyshkin", person.getLastName(), "last name does not match");
     }
 }
