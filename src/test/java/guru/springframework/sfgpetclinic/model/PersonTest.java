38d29061fc0755075851931b8c59fd2b82828b76
admin <d.art.shishkin@gmail.com>
2020-06-10 17:25:39 +0300

    Difference between grouped (assertAll) and non-grouped (just sequence) assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions() {
+        Person person = new Person(1L, "Art", "Shyshkin");
+        assertAll("Test props set",
+                () -> assertEquals(Long.valueOf(2L), person.getId(), "id not match"),
+                () -> assertEquals("Art", person.getFirstName()),
+                () -> assertEquals("Shyshkin1", person.getLastName(), "last name does not match"));
+    }
+
+    @Test
+    void notGroupedAssertions() {
+        Person person = new Person(1L, "Art", "Shyshkin");
+        assertEquals(Long.valueOf(2L), person.getId(), "id not match");
+        assertEquals("Art", person.getFirstName());
+        assertEquals("Shyshkin1", person.getLastName(), "last name does not match");
+    }
+}
