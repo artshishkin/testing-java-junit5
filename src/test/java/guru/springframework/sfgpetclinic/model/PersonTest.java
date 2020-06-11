3e63c4eaef6fc3545692a7f57ff56fb7015d48b9
Art <d.art.shishkin@gmail.com>
2020-06-11 17:22:48 +0300

    Test Dependency Injection (TestInfo, RepetitionInfo, (not used) TestReporter)

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,10 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.RepetitionInfo;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,6 +27,16 @@ class PersonTest implements ModelTests {
     @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} / {totalRepetitions}")
     @DisplayName("Simple Repeated Test")
     void simpleRepeatedTest() {
+    }
+
+    @RepeatedTest(2)
+    @DisplayName("Repeated Test with Dependency Injection")
+    void repeatedTestWithDI(RepetitionInfo repetitionInfo, TestInfo testInfo) {
+        System.out.printf("test repeated %d times, left %d\n", repetitionInfo.getCurrentRepetition(), repetitionInfo.getTotalRepetitions() - repetitionInfo.getCurrentRepetition());
+        System.out.println("testInfo.getDisplayName():" + testInfo.getDisplayName());
+        System.out.println("testInfo.getTags():" + testInfo.getTags());
+        System.out.println("testInfo.getTestClass():" + testInfo.getTestClass());
+        System.out.println("testInfo.getTestMethod():" + testInfo.getTestMethod());
     }
 
 }
