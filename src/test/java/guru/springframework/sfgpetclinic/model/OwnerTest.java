258a8b06859ccaadcb6ffa45b6d33c292a5a162e
Art <d.art.shishkin@gmail.com>
2020-06-12 10:42:12 +0300

    JUnit Parameterized Tests - EnumSource

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -4,6 +4,7 @@ import guru.springframework.sfgpetclinic.ModelTests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,5 +31,12 @@ class OwnerTest implements ModelTests {
     @ValueSource(strings = {"Art", "Kate", "Arina", "Nazar"})
     void testValueSource(String val) {
         System.out.println(val);
+    }
+
+    @DisplayName("Enum Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @EnumSource(value = OwnerType.class)
+    void testEnumSource(OwnerType ownerType) {
+        System.out.println(ownerType);
     }
 }
