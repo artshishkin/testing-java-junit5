2591a011bdb875a6c857075fc6c6136362e3d61a
Art <d.art.shishkin@gmail.com>
2020-06-12 10:31:58 +0300

    JUnit Parameterized Tests - DisplayName

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -24,7 +25,8 @@ class OwnerTest implements ModelTests {
                 ));
     }
 
-    @ParameterizedTest
+    @DisplayName("Value Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
     @ValueSource(strings = {"Art", "Kate", "Arina", "Nazar"})
     void testValueSource(String val) {
         System.out.println(val);
